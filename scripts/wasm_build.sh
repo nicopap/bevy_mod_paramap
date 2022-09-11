527de3926859f27880fb05565821c87dd7115261
Nicola Papale <nico@nicopap.ch>
2022-09-11 12:13:56 +0200

    Make a demo for github pages

--- scripts/wasm_build.sh
+++ scripts/wasm_build.sh
@@ -5,112 +5,71 @@ set -e
 # Pass --run option to run after build (uses python).
 # Files in OutDir is everything needed to run the web page.
 
-OutDir=target/wasm_package
+function build_example {
+	Features="$2"
+	Example="$1"
+	ReleaseFlag="--example $Example $Features"
 
-HttpServerAddress=127.0.0.1
-HttpServerPort=8000
+
+	cargo build --no-default-features \
+		--profile wasm-release \
+		$ReleaseFlag --target wasm32-unknown-unknown
+
+	WasmFile="$(cargo metadata --format-version 1 | sed -n 's/.*"target_directory":"\([^"]*\)".*/\1/p')/wasm32-unknown-unknown/$BuildDir/$Example.wasm"
+
+	if [ ! -e "$WasmFile" ]; then
+		echo "Script is borken, it expects file to exist: $WasmFile"
+		exit 1
+	fi
+
+	BINDGEN_EXEC_PATH="${CARGO_HOME:-~/.cargo}/bin/wasm-bindgen"
+
+	if [ ! -e "$BINDGEN_EXEC_PATH" ] ; then
+	    echo "Please install wasm-bindgen, cannot generate the wasm output without it"
+	    exit 1
+	fi
+
+	$BINDGEN_EXEC_PATH \
+		--no-typescript \
+		--out-dir "$OutDir" \
+		--target web \
+		"$WasmFile"
+
+	if [ -e $(which wasm-opt) ] ; then
+		BindgenOutput="$OutDir/${Example}_bg.wasm"
+		echo "Applying wasm-opt optimizations"
+		echo "before: $(wc -c "$BindgenOutput")"
+		wasm-opt -Oz --output "$BindgenOutput.post-processed" "$BindgenOutput"
+		echo "after : $(wc -c "$BindgenOutput.post-processed")"
+		mv "$BindgenOutput.post-processed" "$BindgenOutput"
+	else
+		echo "Continuing without wasm-opt, it is highly recommended that you"
+		echo "install it, it has been known to divide by two wasm files size"
+	fi
+}
 
 if [ ! -e .git ]; then
 	echo "Must be run from repository root"
 	exit 1
 fi
 
-if [ "$1" = "--debug" ]; then
-	BuildDir="debug"
-	ReleaseFlag=""
-else
-	BuildDir="wasm-release/examples"
-	ReleaseFlag="--example earth3d --features inspector-def --profile wasm-release"
-fi
+OutDir=github_pages
 
-
-#
-# Extract project name from Cargo.toml
-#
-
-ProjName="$(cargo metadata --no-deps --format-version 1 |
-        sed -n 's/.*"name":"\([^"]*\)".*/\1/p')"
-
-#
-# Build
-#
+BuildDir="wasm-release/examples"
 
 if [ ! -e target ] ; then
     mkdir target
 fi
 
-cargo build --no-default-features \
-	$ReleaseFlag --target wasm32-unknown-unknown
-
-WasmFile="$(cargo metadata --format-version 1 | sed -n 's/.*"target_directory":"\([^"]*\)".*/\1/p')/wasm32-unknown-unknown/$BuildDir/$ProjName.wasm"
-
-if [ ! -e "$WasmFile" ]; then
-	echo "Script is borken, it expects file to exist: $WasmFile"
-	exit 1
-fi
-
 [ ! -e "$OutDir" ] || rm -r "$OutDir"
 
-BINDGEN_EXEC_PATH="${CARGO_HOME:-~/.cargo}/bin/wasm-bindgen"
+build_example "earth3d" "--features inspector-def"
+build_example "cube" ""
 
-if [ ! -e "$BINDGEN_EXEC_PATH" ] ; then
-    echo "Please install wasm-bindgen, cannot generate the wasm output without it"
-    exit 1
-fi
+cp scripts/demo_page.html "$OutDir/index.html"
+cp scripts/wasm_build.html "$OutDir/earth3d.html"
+cp scripts/wasm_build.html "$OutDir/cube.html"
+sed -i "s/main.js/earth3d.js/g" "$OutDir/earth3d.html"
+sed -i "s/main.js/cube.js/g" "$OutDir/cube.html"
 
-$BINDGEN_EXEC_PATH \
-	--no-typescript \
-	--out-dir "$OutDir" \
-	--target web \
-	"$WasmFile"
-
-if [ -e $(which wasm-opt) ] ; then
-	BindgenOutput="$OutDir/${ProjName}_bg.wasm"
-	echo "Applying wasm-opt optimizations"
-	echo "before: $(wc -c "$BindgenOutput")"
-	wasm-opt -Os --output "$BindgenOutput.post-processed" "$BindgenOutput"
-	echo "after : $(wc -c "$BindgenOutput.post-processed")"
-	mv "$BindgenOutput.post-processed" "$BindgenOutput"
-else
-	echo "Continuing without wasm-opt, it is highly recommended that you"
-	echo "install it, it has been known to divide by two wasm files size"
-fi
-
-#
-# Copy files
-#
-
-cp scripts/wasm_build.html "$OutDir/index.html"
 cp -r assets "$OutDir/assets"
-
-#
-# bindgen WASM-opt
-#
-
-#
-# Rename JS
-#
-
-Count=0
-for _ in $OutDir/*.js; do
-	((Count+=1))
-done
-
-if [ $Count -ne 1 ]; then
-	echo "Script is broken, must be 1 JS file matching mask"
-	exit 1
-fi
-
-mv $OutDir/*.js "$OutDir/main.js"
-
-#
-# Run
-#
-
-if [ "$1" = "--run" ]; then
-	python3 -m http.server --bind $HttpServerAddress --directory "$OutDir" $HttpServerPort 
-	Job=$!
-    Browser=${BROWSER:-chromium}
-	$Browser http://$HttpServerAddress:$HttpServerPort/index.html
-	kill $Job
-fi
